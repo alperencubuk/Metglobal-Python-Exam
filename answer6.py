f5451e7ee8c492c43fc604723766163d8fff42d8
alperencubuk <alperencubuk@gmail.com>
2019-08-23 18:18:46 +0300

    Fix answer6

--- answer6.py
+++ answer6.py
@@ -2,12 +2,12 @@ def error_handler():
     try:
         name = int('1232a') # ValueError
         lastname = ['yilmaz', 'keskin'][3] # IndexError
-        other_dict = {"name":"merve", "lastname": "demir"} # KeyError
-        other_name = other_dict['other_name'] # NameError
+        other_dict = {"name":"merve", "lastname": "demir"}
+        other_name = other_dict['other_name'] # KeyError
         age = 1/0 # ZeroDivisionError
         print('OK!')
-    except (ValueError, IndexError, KeyError, NameError, ZeroDivisionError):
-        print('Error')
+    except (ValueError, IndexError, KeyError, ZeroDivisionError) as e:
+        print(e)
 
 error_handler()
 
